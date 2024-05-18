423b36fbb25f33c217e08d8c1bfd4524cd9ed032
MartinezFabian <FabianMartinezDev@gmail.com>
2024-05-17 21:54:04 -0300

    Add project

---
+++ src/data_types/booleans.ts
@@ -0,0 +1,20 @@
+let isAuthenticated: boolean = true;
+
+if (isAuthenticated) {
+  console.log('El usuario esta autenticado');
+} else {
+  console.log('El usuario no esta autenticado');
+}
+
+/*
+
+let isDone: boolean;
+
+if (isDone) {
+  console.log('La operacion fue exitosa');
+} else {
+  // en JavaScript entra por el else, en TypeScript da un error
+  console.log('La operacion fallo');
+}
+
+*/
