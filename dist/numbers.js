0af259c40275b9618f6933a153330e78cf18265a
MartinezFabian <FabianMartinezDev@gmail.com>
2024-05-17 22:18:24 -0300

    Add example of number data type in TS

---
+++ dist/numbers.js
@@ -0,0 +1,23 @@
+"use strict";
+/*
+en JS:
+
+let someNumber;
+
+let otherNumber = 10;
+
+if (someNumber < otherNumber) {
+  console.log(`El valor ${someNumber} es menor que ${otherNumber}`);
+  //  En JS imprime en consola 'El valor undefined es mayor que 10'
+} else {
+  console.log(`El valor ${someNumber} es mayor que ${otherNumber}`);
+}
+*/
+let someNumber = 20;
+let otherNumber = 10;
+if (someNumber < otherNumber) {
+    console.log(`El valor ${someNumber} es menor que ${otherNumber}`);
+}
+else {
+    console.log(`El valor ${someNumber} es mayor que ${otherNumber}`);
+}
