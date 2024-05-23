34e190b7fecbff80a22bda0d64e101b6350b4f23
MartinezFabian <FabianMartinezDev@gmail.com>
2024-05-22 21:09:12 -0300

    Add example static methods in TypeScript

---
+++ dist/classes/static.js
@@ -0,0 +1,15 @@
+"use strict";
+class Maths {
+    static add(a, b) {
+        return a + b;
+    }
+    static subtract(a, b) {
+        return a - b;
+    }
+    static multiply(a, b) {
+        return a * b;
+    }
+}
+console.log(Maths.add(10, 5)); // Output: 15
+console.log(Maths.subtract(10, 5)); // Output: 5
+console.log(Maths.multiply(5, 5)); // Output: 25
---
+++ src/classes/static.ts
@@ -0,0 +1,19 @@
+class Maths {
+  static add(a: number, b: number): number {
+    return a + b;
+  }
+
+  static subtract(a: number, b: number): number {
+    return a - b;
+  }
+
+  static multiply(a: number, b: number): number {
+    return a * b;
+  }
+}
+
+console.log(Maths.add(10, 5)); // Output: 15
+
+console.log(Maths.subtract(10, 5)); // Output: 5
+
+console.log(Maths.multiply(5, 5)); // Output: 25
